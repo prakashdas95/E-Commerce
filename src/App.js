a74d1af5d314797a847fe2dff9973d1fc96c3192
Prakash Das <prakashindi01@gmail.com>
2021-01-10 17:13:34 +0530

    header component & links to home,shop page,contact

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
+import Header from "./components/header/header.component";
 
 function App() {
   return (
+    //header is always present and rendered despite whatever 
+    //react - touter - dom and switch component and route component 
+    //decide to render onto the page
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
