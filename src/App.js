1fa17832adf9278e1db258bbc28067f97f27b56a
Prakash Das <prakashindi01@gmail.com>
2021-01-13 22:16:26 +0530

    redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import './App.css';
@@ -36,6 +36,8 @@ class App extends React.Component {
       // if user logout or null then null will be update there
       setCurrentUser(userAuth);
     });
+
+    console.log(this.props.currentUser);
   }
 
   componentWillUnmount() {
@@ -53,15 +55,24 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin'
+            render={() =>
+              this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)
+            }
+          />
         </Switch>
       </div >
     );
   }
 }
 
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
