13144c08d5d97c94e85b2ad9145961eebc1781ce
Prakash Das <prakashindi01@gmail.com>
2021-02-02 19:26:14 +0530

    media queries

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div className={`${size} menu-item`} onClick={() => { history.push(`${match.url}${linkUrl}`) }}>
 
     <div
-      className="background-image" 
+      className="background-image"
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
