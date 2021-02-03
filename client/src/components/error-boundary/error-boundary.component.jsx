c3a854d000a979b6f4139198989bf8cc4fba47a2
Prakash Das <prakashindi01@gmail.com>
2021-02-03 16:34:19 +0530

    error boundary component

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ErrorImageContainer, ErrorImageOverlay, ErrorImageText } from "./error-boundary.styles";
+
+class ErrorBoundary extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            hasErrored: false
+        }
+    }
+
+    // this(getDerivedStateFromError) lifecycle method essentialy catches any error 
+    // that gets thrown in any of the children of this error boundary component.
+    static getDerivedStateFromError(error) {
+        // process the error 
+        return { hasErrored: true };
+    }
+
+    // second lifecycle method we actually have access to that 
+    // lets react know that this component is a error boundary component
+    componentDidCatch(error, info) {
+        console.log(error);
+    }
+
+    render() {
+        if (this.state.hasErrored) {
+            return (
+                <ErrorImageContainer>
+                    <ErrorImageContainer imageUrl='https://i.imgur.com/yW2W9SC.png' />
+                    <ErrorImageText>Sorry this page is broken</ErrorImageText>
+                </ErrorImageContainer>
+            )
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
